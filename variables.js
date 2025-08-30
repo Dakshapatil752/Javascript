4c3396c53872d2704439f15008c676165594fb77
DakshaPatil <dakshap0205@gmail.com>
2025-08-30 19:13:43 +0530

    Add new files

---
+++ variables.js
@@ -0,0 +1,17 @@
+console.log("code for ultimate javascript course")
+var a = 67
+console.log(a)
+a ="Harry"
+console.log(a)
+
+// The value of javascript can be changed during the execution of the a program
+
+// let 8Harry = 7 // not allowed this throw an error
+//let var = 7 // reserverd keywords not allowed
+
+// var vs let in javascript
+   //var is globally scoped while let & const are block scoped
+   // var can be updated and declared within the scope
+   //let can be updated but not re-declared
+   // const can neither be updated nor be re-declared
+   // 
