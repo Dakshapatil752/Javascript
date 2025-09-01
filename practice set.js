1a66180f57fa2de02b7f118d9a72daa25c4872a9
DakshaPatil <dakshap0205@gmail.com>
2025-09-01 22:25:09 +0530

    Add new files

---
+++ practice set.js
@@ -0,0 +1,29 @@
+// Q-1 - create a  variables of type string try to add a number to it
+let a ="xyz"
+let b=6
+console.log(a+b)
+
+// Q-2 use typeof operator to find the datatype of the string in last question
+console.log(typeof a+b)
+
+//Q-3 create the object a javascript can you change it to hold a number later
+const c = {
+    namr:"xyz",
+    section:1,
+    isPrincipal:false
+}
+// a1=45  a1={} 
+
+//Q-4 try to add new key to the const object in prob 3 
+//a1["friend"]="Digvijay"
+//a1["name"]="Lovish"
+//console.log(a1)
+
+//Q-5 write the program to create a word meaning dictionary of 5 words
+const dict =
+{
+    appreciate:"recognize the full worth of",
+    ataraxia:"a state of freedom emotional distrubance and anxiety",
+    yakka:"work, especially hard work",
+}
+console.log(dict.yakka)
