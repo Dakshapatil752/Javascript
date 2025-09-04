c6726fc93c45f8cf73543fe135234a1eea5c3841
DakshaPatil <dakshap0205@gmail.com>
2025-09-04 22:21:22 +0530

    Add new files

---
+++ expression&operator.js
@@ -0,0 +1,38 @@
+// A fragment of code that produces a value is called an expression 
+
+77;
+"Daksha"
+let a= 45;
+let b=4;
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a*b=",a*b)
+console.log("a/b=",a/b)
+console.log("a**b=",a**b)
+console.log("a%b=",a%b)
+console.log("a++=",a++)
+console.log("a=",a)
+console.log("a--=",a--)
+
+//assignment operator
+a+=5  // same as a =a+5
+console.log(a)
+a/=5
+console.log("a is now =",a)
+
+// comparision operator
+let comp1 =6 ;
+let comp2 = 7;
+console.log("com1 == comp2 is",comp1==comp2)
+console.log("com1 == comp2 is",comp1!=comp2)
+console.log("com1 === comp2 is",comp1===comp2)
+console.log("com1 !== comp2 is",comp1!==comp2)
+
+//logical operator
+let x =5;
+let y =6;
+console.log(x<y && x==5)
+console.log(x>y && x==5)
+console.log(!false)
+console.log(!true)
+
